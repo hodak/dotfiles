d4464a6b35859bac1d6be70f8e895414ef7c858b
Dariusz Gertych <dariusz.gertych@gmail.com>
2013-11-10 16:46:20 +0100

    Remove boxes setup, move scripts to bin directory

--- script/boxen-setup.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-echo "boxen setup ..."
-sudo mkdir -p /opt/boxen
-sudo chown ${USER}:staff /opt/boxen
-git clone https://github.com/chytreg/our-boxen.git /opt/boxen/repo
-cd /opt/boxen/repo
-bundle install
-script/boxen --debug
--- script/check-boxen-puppet.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-curl -s https://github.com/boxen/puppet-$1/tags | grep tag-name | head -n 1 | grep -o  '\d\.\d\.\d'
