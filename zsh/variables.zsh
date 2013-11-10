393ef169d3783f1ba92eb2f8cdda06542f55e9c0
Dariusz Gertych <dariusz.gertych@gmail.com>
2013-11-10 16:47:05 +0100

    Add bootstrap script

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -32,3 +32,6 @@ export LD_LIBRARY_PATH=/opt/boxen/homebrew/lib
 # Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
 # Remove -X and -F (exit if the content fits on one screen) to enable it.
 export LESS='-F -g -i -M -R -S -w -X -z-4'
+
+# Homebrew cask
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
