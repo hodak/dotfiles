2d891e2ff25a4ec4351b1e8a779525dbd681163c
hodak <kaczor555@gmail.com>
2015-02-08 19:46:56 +0100

    Revert "Use brew services instead of custom scripts"
    brew services has been deprecated
    
    This reverts commit e4f2688a8957d54e067e366f2f442fe441ff8bc1.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -52,6 +52,46 @@ function project {
   fi
 }
 
+function services() {
+  if [ -z $agent ]; then
+    echo "Service: $1 not found"
+  else
+    case "$2" in
+    "start")
+      launchctl load -w $service_path/$agent
+      ;;
+    "stop")
+      launchctl unload $service_path/$agent
+      ;;
+    "reload")
+      launchctl unload $service_path/$agent
+      launchctl load -w $service_path/$agent
+      ;;
+    *)
+      echo "Usage: $0 $1 start|stop|reload"
+      ;;
+    esac
+  fi
+}
+
+# agents name start|stop|reload|install
+
+function agents(){
+: ${1?"Usage: agents name start|stop|reload|install"}
+  service_path=~/Library/LaunchAgents
+  if [ $2 = 'install' ]; then
+    if (brew --prefix $1 > /dev/null); then
+      ln -sfv $(brew --prefix $1)/*.plist $service_path
+      chmod 644 $(brew --prefix $1)/*.plist
+      agent=$(ls -L $service_path | grep $1 | head -n 1)
+      services $1 'start'
+    fi
+  else
+    agent=$(ls -L $service_path | grep $1 | head -n 1)
+    services $1 $2
+  fi
+}
+
 function remove-backups() {
   ls -d ~/* | grep '.*bak' | xargs -I {} rm -rf {}
 }
