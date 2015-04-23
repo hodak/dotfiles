87ba5ab9fef3d54e88671308ae8b0bec76074d59
hodak <kaczor555@gmail.com>
2015-04-23 14:52:49 +0200

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,11 +10,12 @@ alias gr="g r"
 alias g="git"
 alias be="bundle exec"
 alias rc="be rails c"
-alias rt="touch tmp/restart.txt"
+alias rt='echo "restarting"; touch tmp/restart.txt; spring stop'
 alias grep="grep --color"
 alias vi="vim" # lame, i know
 alias c="clear"
-alias rspec="clear; bundle exec rspec --color"
+alias rspec="clear; bundle exec rspec --color --format documentation"
+alias bsp="./bin/rspec"
 alias st="sky run tail -f log/staging.log"
 alias dt="tail -F -n 0 log/development.log"
 
