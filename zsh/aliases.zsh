1292e66c25e2b98e0176428235f6e021d05f3fe1
hodak <kaczor555@gmail.com>
2016-05-05 08:33:39 +0200

    Add vanilla vim (without plugins) as alias
    * It's usefil with very large files where plugins slow everything down

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,6 +15,7 @@ alias rc="be rails c"
 alias rt='echo "restarting"; touch tmp/restart.txt; spring stop'
 alias grep="grep --color"
 alias vi="vim" # lame, i know
+alias vvim="vim -u NONE" # vanilla vim
 alias c="clear"
 alias rspec="clear; bundle exec rspec --color --format documentation"
 alias bsp="./bin/rspec"
