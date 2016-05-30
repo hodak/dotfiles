249c5716f064e9e12d8b3722cfcecac289aae15e
hodak <kaczor555@gmail.com>
2016-05-30 18:55:52 +0200

    Comment-out JAVA-related variables
    * Not planning right now ;)

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -5,7 +5,7 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X
 if which rbenv > /dev/null; then eval "$(rbenv init - zsh --no-rehash)"; fi
 
 # == java-opts ==
-export JAVA_OPTS="-Xms256m -Xmx512m -Dfile.encoding=UTF-8 -noverify"
+# export JAVA_OPTS="-Xms256m -Xmx512m -Dfile.encoding=UTF-8 -noverify"
 
 # == browser ==
 if [[ "$OSTYPE" == darwin* ]]; then export BROWSER='open'; fi
@@ -19,7 +19,7 @@ export VISUAL='vim'
 export PAGER='less'
 
 # == java home ==
-export JAVA_HOME=$(/usr/libexec/java_home)
+# export JAVA_HOME=$(/usr/libexec/java_home)
 
 # == less ==
 # Set the default Less options.
