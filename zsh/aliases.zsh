097065d646a8a535d5714f09e0bb21c64e7b81f1
hodak <kaczor555@gmail.com>
2018-08-12 16:38:11 +0200

    Aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,11 +5,13 @@ alias ..3="cd ../../.."
 alias ..4="cd ../../../.."
 alias ..5="cd ../../../../.."
 
+alias ghostscript="/usr/local/bin/gs"
 alias gs="g st"
 alias gr="g r"
 alias g="git"
 alias gd="g di"
 alias gdc="gd --cached"
+alias gdf="g df"
 alias be="bundle exec"
 alias rc="be rails c"
 alias rt='echo "restarting"; touch tmp/restart.txt; spring stop'
@@ -31,3 +33,5 @@ alias simpleserver="open http://localhost:8000; python -m SimpleHTTPServer"
 alias flushdns='dscacheutil -flushcache;sudo killall -HUP mDNSResponder;say flushed'
 
 alias priex="iex --sname pry"
+
+alias tree="tree -I 'node_modules'"
