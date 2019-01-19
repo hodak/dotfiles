b2a13ff6efd0173c801e8be75f331be963d0b475
hodak <kaczor555@gmail.com>
2019-01-19 12:29:36 +0100

    Remove JAVA from variables

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -4,9 +4,6 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X
 # == rbenv ==
 if which rbenv > /dev/null; then eval "$(rbenv init - zsh --no-rehash)"; fi
 
-# == java-opts ==
-export JAVA_OPTS="-Xms256m -Xmx512m -Dfile.encoding=UTF-8 -noverify"
-
 # == browser ==
 if [[ "$OSTYPE" == darwin* ]]; then export BROWSER='open'; fi
 
@@ -17,9 +14,6 @@ if [[ -z "$LANG" ]]; then export LANG='en_US.UTF-8'; fi
 export EDITOR='vim'
 export VISUAL='vim'
 export PAGER='less'
-
-# == java home ==
-export JAVA_HOME=$(/usr/libexec/java_home)
 
 # == less ==
 # Set the default Less options.
