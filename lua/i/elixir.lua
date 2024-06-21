71bfa515ddb8118a214a8ea37eab0bbbeae75e26
Adam Hodowany <kaczor555@gmail.com>
2024-06-21 13:46:04 +0200

    [nvim] Adjust elixir configuration

--- lua/i/elixir.lua
+++ lua/i/elixir.lua
@@ -1,9 +1,25 @@
 -- elixir-tools
-require('elixir').setup({
+
+local elixir = require("elixir")
+local elixirls = require("elixir.elixirls")
+
+elixir.setup({
   elixirls = {
     enable = true,
     -- tail -f ~/.local/state/nvim/lsp.log
     cmd = '/Users/adam/elixir-ls/language_server.sh',
+    -- default settings, use the `settings` function to override settings
+    settings = elixirls.settings {
+      dialyzerEnabled = true,
+      fetchDeps = true,
+      enableTestLenses = true,
+      suggestSpecs = true,
+    },
+    on_attach = function(client, bufnr)
+      vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+      vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+      vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+    end
   },
   nextls = {
     enable = false,
@@ -13,7 +29,7 @@ require('elixir').setup({
       }
     }
   },
-  credo = { enable = true },
+  credo = { enable = false },
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', { pattern = '*.ex,*.exs', command = 'lua vim.lsp.buf.format({ async = false })' })
